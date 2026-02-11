017712c4adea01848d59654a5380402e064c26fb
Seiya <silmin4@proton.me>
2026-02-11 14:27:41 +0900

    initial commit

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    set -g fish_greeting
+    set -g fish_color_command green
+
+    bind \cn accept-autosuggestion
+
+    alias ls 'eza'
+    alias l 'eza -l'
+    alias ll 'eza -lh'
+    alias la 'eza -lha'
+end
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,38 @@
+function fish_prompt
+    set -l last_status $status
+
+    printf '\n'
+
+    # user@hostname
+    set_color blue
+    printf '%s@%s' $USER (prompt_hostname)
+    set_color normal
+
+    # path
+    set -g fish_prompt_pwd_dir_length 3
+    printf ' '
+    set_color bryellow
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    # git
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_showstashstate 1
+    set -g __fish_git_prompt_color brmagenta
+    set -g __fish_git_prompt_color_branch brmagenta
+    set -g __fish_git_prompt_color_dirtystate brred
+    set -g __fish_git_prompt_color_untrackedfiles brred
+    set -g __fish_git_prompt_color_stashstate brcyan
+    printf '%s' (fish_git_prompt)
+    set_color normal
+
+    # prompt symbol
+    if test $last_status -ne 0
+        set_color brred
+    else
+        set_color normal
+    end
+    printf '\n$ '
+    set_color normal
+end
