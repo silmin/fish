45f46b6fc3eaf7ca5ea379c82f05f724afe3c808
Seiya <silmin4@proton.me>
2026-02-17 03:43:53 +0900

    reverse ctrl-n and ctrl-p

--- config.fish
+++ config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     set -g fish_greeting
     set -g fish_color_command green
 
+    bind ctrl-n history-prefix-search-backward
+    bind ctrl-p history-prefix-search-forward
+
     alias ls 'eza'
     alias l 'eza -l'
     alias ll 'eza -lh'
